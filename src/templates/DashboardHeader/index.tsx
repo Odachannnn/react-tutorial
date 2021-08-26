c2dadb1b192decfdc19bd9421e66536cad2cb255
織田麻子 <12odadao14@gmail.com>
2021-08-26 23:05:54 +0900

    トップバーまで作成

---
+++ src/templates/DashboardHeader/index.tsx
@@ -0,0 +1,38 @@
+import { AppBar, Avatar, Grid, Toolbar, IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import { Logo } from "../../components/Logo";
+import { SearchBar } from "./SearchBar"
+
+import useStyles from "./style"
+
+export const DashboardHeader = () => {
+    const styles = useStyles();
+    return (
+        <AppBar elevation={0} color="inherit">
+            <Toolbar className={styles.between}>
+                <div className={styles.flex}>
+                    <IconButton>
+                        <MenuIcon />
+                    </IconButton>
+                    <div className={styles.logo}>
+                        <Logo />
+                    </div>
+                </div>
+
+                <SearchBar />
+
+                <div className={styles.flex}>
+                    <IconButton>
+                        <VideoCallIcon />
+                    </IconButton>
+
+                    <IconButton className={styles.profileIcon}>
+                        <Avatar />
+                    </IconButton>
+                </div>
+
+            </Toolbar>
+        </AppBar>
+    )
+};
