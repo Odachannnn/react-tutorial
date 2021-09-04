b531bf6aece954d9cb40cc04726f326bd2c143bd
織田麻子 <12odadao14@gmail.com>
2021-09-04 16:57:03 +0900

    @apollo/clientインストール

--- src/utils/graphql/generated.ts
+++ src/utils/graphql/generated.ts
@@ -1,7 +1,10 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -808,3 +811,95 @@ export type Videos_Variance_Fields = {
   owner_id?: Maybe<Scalars['Float']>;
   views?: Maybe<Scalars['Float']>;
 };
+
+export type InsertUserMutationVariables = Exact<{
+  name: Scalars['String'];
+}>;
+
+
+export type InsertUserMutation = { __typename?: 'mutation_root', insert_users_one?: Maybe<{ __typename?: 'users', created_at: any, id: number, name: string, profile_photo_url: string, updated_at: any }> };
+
+export type SelectUserByIdQueryVariables = Exact<{
+  id: Scalars['Int'];
+}>;
+
+
+export type SelectUserByIdQuery = { __typename?: 'query_root', users_by_pk?: Maybe<{ __typename?: 'users', created_at: any, id: number, name: string, profile_photo_url: string, updated_at: any }> };
+
+
+export const InsertUserDocument = gql`
+    mutation InsertUser($name: String!) {
+  insert_users_one(object: {name: $name, profile_photo_url: ""}) {
+    created_at
+    id
+    name
+    profile_photo_url
+    updated_at
+  }
+}
+    `;
+export type InsertUserMutationFn = Apollo.MutationFunction<InsertUserMutation, InsertUserMutationVariables>;
+
+/**
+ * __useInsertUserMutation__
+ *
+ * To run a mutation, you first call `useInsertUserMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useInsertUserMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [insertUserMutation, { data, loading, error }] = useInsertUserMutation({
+ *   variables: {
+ *      name: // value for 'name'
+ *   },
+ * });
+ */
+export function useInsertUserMutation(baseOptions?: Apollo.MutationHookOptions<InsertUserMutation, InsertUserMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<InsertUserMutation, InsertUserMutationVariables>(InsertUserDocument, options);
+      }
+export type InsertUserMutationHookResult = ReturnType<typeof useInsertUserMutation>;
+export type InsertUserMutationResult = Apollo.MutationResult<InsertUserMutation>;
+export type InsertUserMutationOptions = Apollo.BaseMutationOptions<InsertUserMutation, InsertUserMutationVariables>;
+export const SelectUserByIdDocument = gql`
+    query SelectUserById($id: Int!) {
+  users_by_pk(id: $id) {
+    created_at
+    id
+    name
+    profile_photo_url
+    updated_at
+  }
+}
+    `;
+
+/**
+ * __useSelectUserByIdQuery__
+ *
+ * To run a query within a React component, call `useSelectUserByIdQuery` and pass it any options that fit your needs.
+ * When your component renders, `useSelectUserByIdQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useSelectUserByIdQuery({
+ *   variables: {
+ *      id: // value for 'id'
+ *   },
+ * });
+ */
+export function useSelectUserByIdQuery(baseOptions: Apollo.QueryHookOptions<SelectUserByIdQuery, SelectUserByIdQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<SelectUserByIdQuery, SelectUserByIdQueryVariables>(SelectUserByIdDocument, options);
+      }
+export function useSelectUserByIdLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<SelectUserByIdQuery, SelectUserByIdQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<SelectUserByIdQuery, SelectUserByIdQueryVariables>(SelectUserByIdDocument, options);
+        }
+export type SelectUserByIdQueryHookResult = ReturnType<typeof useSelectUserByIdQuery>;
+export type SelectUserByIdLazyQueryHookResult = ReturnType<typeof useSelectUserByIdLazyQuery>;
+export type SelectUserByIdQueryResult = Apollo.QueryResult<SelectUserByIdQuery, SelectUserByIdQueryVariables>;
