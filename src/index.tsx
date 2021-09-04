d1f2520a3e18d934fceec9054e1bd9bdf13b305d
織田麻子 <12odadao14@gmail.com>
2021-09-04 17:52:53 +0900

    Apollo Clientでのデータフェッチ追加

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,45 @@ import { RootRouter } from "./Route";
 import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
 import GlobalStyle from './GlobalStyle';
 
+import {
+	ApolloProvider,
+	ApolloClient,
+	createHttpLink,
+	InMemoryCache
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 const theme = createTheme();
+const httpLink = createHttpLink({
+	uri: process.env.REACT_APP_GRAPHQL_END_POINT,
+});
+// GraphQLのリクエストを送信する際に付与するRequest Headersなどをここで指定する
+// 本来であれば認証情報などをここで取得し、トークンをHeadersに付与する。
+const authLink = setContext(async () => {
+	return {
+		headers: {
+			"x-hasura-admin-secret": process.env.REACT_APP_X_HASURA_ADMIN_SECRET,
+		}
+	};
+});
+
+const apolloClient = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
   	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<BrowserRouter>
-				<CssBaseline />
-				<GlobalStyle />
-				<RootRouter />
-			</BrowserRouter>
+			<ApolloProvider client={apolloClient}>
+				<BrowserRouter>
+					<CssBaseline />
+					<GlobalStyle />
+					<RootRouter />
+				</BrowserRouter>
+			</ApolloProvider>
 		</ThemeProvider>
   	</React.StrictMode>,
   document.getElementById('root')
 );
+
--- src/templates/DashboardHeader/index.tsx
+++ src/templates/DashboardHeader/index.tsx
@@ -1,13 +1,32 @@
-import { AppBar, Avatar, Grid, Toolbar, IconButton } from "@material-ui/core";
+import { 
+    AppBar,
+    Avatar, 
+    Toolbar,
+    IconButton,
+    Typography
+} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
 import { Logo } from "../../components/Logo";
 import { SearchBar } from "./SearchBar"
+import { useSelectUserByIdQuery } from "../../utils/graphql/generated";
+import { useEffect } from "react";
 
 import useStyles from "./style"
 
 export const DashboardHeader = () => {
     const styles = useStyles();
+
+    const { data, error } = useSelectUserByIdQuery({
+        variables: { "id": 1 },
+    });
+
+    useEffect(() => {
+        console.log(data);
+        console.log(error);
+    }, [data]);
+    
+    
     return (
         <AppBar elevation={0} color="inherit">
             <Toolbar className={styles.between}>
@@ -23,6 +42,9 @@ export const DashboardHeader = () => {
                 <SearchBar />
 
                 <div className={styles.flex}>
+                    <IconButton>
+                        <Typography>{data?.users_by_pk?.name}</Typography>
+                    </IconButton>
                     <IconButton>
                         <VideoCallIcon />
                     </IconButton>
